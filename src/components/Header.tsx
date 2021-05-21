f9de18a9c1a17a26e9c2f389bfa5b048abde15b1
Mondal10 <amitmondal111.am@gmail.com>
2021-05-22 01:24:56 +0530

    image added, header created, homepage update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextStyle, View, ViewStyle } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function Header() {
-    return (
-        <View>
-            <Text>Header</Text>
-        </View>
-    )
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Weather App</Text>
+			<MaterialCommunityIcons name="reload" size={24} color="black" />
+		</View>
+	)
 }
 
-const styles = StyleSheet.create({
-    
+// Todo: make a separate styles interface file and import
+interface IStyle {
+	container: ViewStyle;
+	title: TextStyle;
+	reload: ViewStyle;
+};
+
+const styles = StyleSheet.create<IStyle>({
+	container: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: 20,
+		marginBottom: 10,
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: 'bold',
+	},
+	reload: {
+
+	}
 });
 
 export default Header;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,115 +5,121 @@ import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo
 import { PermissionStatus, Units } from '../shared/enums';
 
 import { getWeatherByLatLong, getWeatherByCityName } from '../api/fetchUrls';
+import Header from '../components/Header';
 
 const dummy = {
-    "coord": {
-        "lon": 72.8479,
-        "lat": 19.0144
-    },
-    "weather": [
-        {
-            "id": 500,
-            "main": "Rain",
-            "description": "light rain",
-            "icon": "10d"
-        },
-        {
-            "id": 711,
-            "main": "Smoke",
-            "description": "smoke",
-            "icon": "50d"
-        }
-    ],
-    "base": "stations",
-    "main": {
-        "temp": 308.66,
-        "feels_like": 311.66,
-        "temp_min": 308.15,
-        "temp_max": 309.15,
-        "pressure": 996,
-        "humidity": 41
-    },
-    "visibility": 5000,
-    "wind": {
-        "speed": 3.6,
-        "deg": 70
-    },
-    "rain": {
-        "1h": 0.6
-    },
-    "clouds": {
-        "all": 75
-    },
-    "dt": 1621159099,
-    "sys": {
-        "type": 1,
-        "id": 9052,
-        "country": "IN",
-        "sunrise": 1621125234,
-        "sunset": 1621172174
-    },
-    "timezone": 19800,
-    "id": 1275339,
-    "name": "Mumbai",
-    "cod": 200
+	"coord": {
+		"lon": 72.8479,
+		"lat": 19.0144
+	},
+	"weather": [
+		{
+			"id": 500,
+			"main": "Rain",
+			"description": "light rain",
+			"icon": "10d"
+		},
+		{
+			"id": 711,
+			"main": "Smoke",
+			"description": "smoke",
+			"icon": "50d"
+		}
+	],
+	"base": "stations",
+	"main": {
+		"temp": 308.66,
+		"feels_like": 311.66,
+		"temp_min": 308.15,
+		"temp_max": 309.15,
+		"pressure": 996,
+		"humidity": 41
+	},
+	"visibility": 5000,
+	"wind": {
+		"speed": 3.6,
+		"deg": 70
+	},
+	"rain": {
+		"1h": 0.6
+	},
+	"clouds": {
+		"all": 75
+	},
+	"dt": 1621159099,
+	"sys": {
+		"type": 1,
+		"id": 9052,
+		"country": "IN",
+		"sunrise": 1621125234,
+		"sunset": 1621172174
+	},
+	"timezone": 19800,
+	"id": 1275339,
+	"name": "Mumbai",
+	"cod": 200
 };
 
 function Home() {
-    const [errorMsg, setErrorMsg] = useState<null | string>(null);
-    const [weatherData, setWeatherData] = useState<null | any>(null);
-    const [unit, setUnit] = useState<Units>(Units.METRIC);
+	const [errorMsg, setErrorMsg] = useState<null | string>(null);
+	const [weatherData, setWeatherData] = useState<null | any>(null);
+	// const [unit, setUnit] = useState<Units>(Units.METRIC);
 
-    useEffect(() => {
-        load();
-    }, [])
+	useEffect(() => {
+		load();
+	}, [])
 
-    async function load() {
-        try {
-            const { status } = await requestForegroundPermissionsAsync();
+	async function load() {
+		try {
+			const { status } = await requestForegroundPermissionsAsync();
 
-            if (status !== PermissionStatus.GRANTED) {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
+			if (status !== PermissionStatus.GRANTED) {
+				setErrorMsg('Permission to access location was denied');
+				return;
+			}
 
-            const location = await getCurrentPositionAsync({});
-            const { latitude, longitude } = location.coords;
-            // const response = await getWeatherByLatLong(latitude, longitude, unit);
+			const location = await getCurrentPositionAsync({});
+			const { latitude, longitude } = location.coords;
+			// const response = await getWeatherByLatLong(latitude, longitude, unit);
 
-            if (response.cod === 200) {
-                setWeatherData(response);
-            } else {
-                setErrorMsg(response.message);
-            }
-        } catch (error) {
-            setErrorMsg(error.message);
-            throw new Error(error);
-        }
-    }
+			// if (response.cod === 200) {
+			//     setWeatherData(response);
+			// } else {
+			//     setErrorMsg(response.message);
+			// }
+		} catch (error) {
+			setErrorMsg(error.message);
+			throw new Error(error);
+		}
+	}
 
-    if (weatherData) {
-        return ( 
-            <View>
-                <Text>{weatherData.main.temp}</Text>
-            </View>
-        )
-    } else {
-        return (
-            <View>
-                <Text>{errorMsg}</Text>
-            </View>
-        )
-    }
+	return (
+		<View style={styles.home}>
+			<Header />
+			{
+				(true || weatherData) ?
+					<>
+						{/* Weather Screen */}
+						<Text>{Math.round(29.93)}°</Text>
+						{/* <Text>{weatherData.main.temp}</Text> */}
+					</> :
+					<>
+						{/* Error Screen */}
+						<Text>{errorMsg}</Text>
+					</>
+			}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    home: {
-        //   flex: 1,
-        //   backgroundColor: '#fff',
-        //   alignItems: 'center',
-        //   justifyContent: 'center',
-    },
+	home: {
+		display: 'flex',
+		width: '100%',
+		height: '100%',
+		borderColor: 'red',
+		borderWidth: 1,
+	},
 });
 
 export default Home;
