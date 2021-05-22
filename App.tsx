36b955bf0174b45a3d4079140d6fee7b34c0105f
Mondal10 <amitmondal111.am@gmail.com>
2021-05-22 14:09:06 +0530

    linear gradient applied on background

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import Home from './src/screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      locations={[0.1, 0.5, 1]}
+      colors={['#faf7fe', '#f5dffa', '#faf7fe']}
+      style={styles.container}
+    >
       <StatusBar style="auto" />
       <Home />
-    </View>
+    </LinearGradient>
   );
 }
 
